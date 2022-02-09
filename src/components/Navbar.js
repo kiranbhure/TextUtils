d62845488b180ae1cc4442a6efa1b20e638fdf36
kbhure4@gmail.com <kbhure4@gmail.com>
2022-02-09 15:43:52 +0530

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navebar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link" aria-current="page" href="#">Home</a>
               {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
             </li>
             <li className="nav-item">
--- src/components/TextForms.js
+++ src/components/TextForms.js
@@ -51,7 +51,7 @@ export default function TextForms(props) {
         </div>
         <div className="container my-3" style={{color:props.mode === 'dark'?'white':'#042743'}}>
         <h2>Your Text Summery</h2>
-        <p>{(text.split(" ").filter((element)=>{return element.length !==0 }).length)} Wordsand {text.length} Characters</p>
+        <p>{(text.split(/\s+/).filter((element)=>{return element.length !==0 }).length)} Wordsand {text.length} Characters</p>
         <p> {(0.008 * text.split(" ").filter((element)=>{return element.length !==0 }).length)} Minutes to read</p>
         <h3>Preview</h3>
         <p>{text.length > 0 ? text:'Nothing to preview'}</p>
